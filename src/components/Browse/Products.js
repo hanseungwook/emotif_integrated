24276a29cf1d782b7c0e0c51691eb2843e57a10c
Seungwook Han <Seungwook@dyn-160-39-142-159.dyn.columbia.edu>
2017-03-30 19:15:54 -0400

    Changes to browse

--- src/components/Browse/Products.js
+++ src/components/Browse/Products.js
@@ -1,7 +1,5 @@
 import React, { Component } from 'react';
-import img1 from '../../../images/browse/red-dress.jpg';
-import img2 from '../../../images/browse/red-dress.jpg';
-import img3 from '../../../images/browse/red-dress.jpg';
+const img1 = require('../../../images/browse/red-dress.jpg');
 
 export default class Products extends Component {
     render() {
@@ -9,28 +7,28 @@ export default class Products extends Component {
             <div id='product-page'>
                     <div className='product'>
                         <div className='product-img'>  <img src={img1} alt={"Product"} /> </div>
-                        <div className='product-title'> Red Dress </div>
-                        <div className='product-designer'> Alexander Wang </div>
+                        <div className='product-title'> Pastel Red Dress </div>
+                        <div className='product-designer'> by Alexander Wang </div>
                     </div>
                     <div className='product'>
-                        <div className='product-img'>  <img src={img2} alt={"Product"} /> </div>
-                        <div className='product-title'> Red Dress </div>
-                        <div className='product-designer'> Alexander Wang </div>
+                        <div className='product-img'>  <img src={img1} alt={"Product"} /> </div>
+                        <div className='product-title'> Pastel Red Dress </div>
+                        <div className='product-designer'> by Alexander Wang </div>
                     </div>
                     <div className='product'>
-                        <div className='product-img'>  <img src={img3} alt={"Product"} /> </div>
-                        <div className='product-title'> Red Dress </div>
-                        <div className='product-designer'> Alexander Wang </div>
+                        <div className='product-img'>  <img src={img1} alt={"Product"} /> </div>
+                        <div className='product-title'> Pastel Red Dress </div>
+                        <div className='product-designer'> by Alexander Wang </div>
                     </div>
                     <div className='product'>
-                        <div className='product-img'>  <img src={img3} alt={"Product"} /> </div>
-                        <div className='product-title'> Red Dress </div>
-                        <div className='product-designer'> Alexander Wang </div>
+                        <div className='product-img'>  <img src={img1} alt={"Product"} /> </div>
+                        <div className='product-title'> Pastel Red Dress </div>
+                        <div className='product-designer'> by Alexander Wang </div>
                     </div>
                     <div className='product'>
-                        <div className='product-img'>  <img src={img3} alt={"Product"} /> </div>
-                        <div className='product-title'> Red Dress </div>
-                        <div className='product-designer'> Alexander Wang </div>
+                        <div className='product-img'>  <img src={img1} alt={"Product"} /> </div>
+                        <div className='product-title'> Pastel Red Dress </div>
+                        <div className='product-designer'> by Alexander Wang </div>
                     </div>
             </div>
         );
--- src/containers/browse/BrowseContainer.js
+++ src/containers/browse/BrowseContainer.js
@@ -1,14 +1,5 @@
-import React, { Component, PropTypes } from 'react';
-import { connect } from 'react-redux';
-import { bindActionCreators } from 'redux';
+import React, { Component } from 'react';
 import BrowsePage from '../../components/Browse/BrowsePage';
-const Scroll = require('react-scroll');
-
-const Link       = Scroll.Link;
-const Element    = Scroll.Element;
-const Events     = Scroll.Events;
-const scroll     = Scroll.animateScroll;
-const scrollSpy  = Scroll.scrollSpy;
 
 export class BrowseContainer extends Component {
     constructor(props) {
@@ -17,44 +8,11 @@ export class BrowseContainer extends Component {
 
     componentDidMount() {
         console.log("Did mount");
-
-        Events.scrollEvent.register('begin', function(to, element) {
-            console.log("begin", to, element);
-        });
-
-        Events.scrollEvent.register('end', function(to, element) {
-            console.log("end", to, element);
-        });
-
-        scrollSpy.update();
     }
 
     componentWillUnmount() {
         console.log("Will unmount");
-        Events.scrollEvent.remove('begin');
-        Events.scrollEvent.remove('end');
     }
-
-    scrollToTop() {
-        scroll.scrollToTop();
-    }
-
-    scrollToBottom() {
-        scroll.scrollToBottom();
-    }
-
-    scrollTo() {
-        scroll.scrollTo(100);
-    }
-
-    scrollMore() {
-        scroll.scrollMore(100);
-    }
-
-    handleSetActive() {
-        console.log(to);
-    }
-
     render() {
         return (
             <BrowsePage />
