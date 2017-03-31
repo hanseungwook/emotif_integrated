0c07bc6893a12ce8bb5e4c126558544894ff2215
Matthieu Gavaudan <mlg2199@columbia.edu>
2017-03-31 04:51:00 -0400

    fixed reducer problem

--- src/containers/landing/landingReducers.js
+++ src/containers/landing/landingReducers.js
@@ -1,6 +1,6 @@
-import { WAITLIST_SUCCESS, WAITLIST_FAILURE } from './landingActions';
+import { WAITLIST_SUCCESS, WAITLIST_FAILURE, CHANGE_PANE } from './landingActions';
 
-const landingReducer = ( state = {isFetching: false,
+const landing = ( state = { paneId:1, isFetching: false,
 	isAuthenticated: localStorage.getItem('id_token') ? true : false }, action) =>
 {
 	switch (action.type) {
@@ -16,9 +16,13 @@ const landingReducer = ( state = {isFetching: false,
 				isAuthenticated: false,
 				errorMessage: ''
 			});
+		case CHANGE_PANE:
+			return Object.assign({}, state, {
+				paneId: action.id
+			});
 		default:
 			return state;
 	}
 };
 
-export default landingReducer;
+export default landing;
