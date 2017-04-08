9ea29928718c70c4db474134eb44e84ed71a20be
Seungwook Han <Seungwook@dyn-160-39-143-239.dyn.columbia.edu>
2017-04-07 23:21:34 -0400

    Developing stripe tests

--- backend/test/user.js
+++ backend/test/user.js
@@ -58,6 +58,7 @@ describe('/POST /classes/_User', () => {
 });
 
 
+/*
 // Successful user authentication
 describe('/GET /login', () => {
     it('it should give successfully login user', (done) => {
@@ -81,6 +82,7 @@ describe('/GET /login', () => {
             });
     });
 });
+*/
 
 // Successful waitlist
 describe('/POST /classes/waitlist', () => {
@@ -103,6 +105,7 @@ describe('/POST /classes/waitlist', () => {
             });
     });
 });
+
 
 // Successful createStripetoken
 describe('/POST /functions/createStripeToken', () => {
@@ -132,5 +135,75 @@ describe('/POST /functions/createStripeToken', () => {
     });
 });
 
+// Successful createStripeCustomer
+describe('/POST /functions/createStripeCustomer', () => {
+    it('it should successfully create a stripe customer', (done) => {
+        let card = {
+            email: 'josh1@gmail.com',
+            number: 4242424242424242,
+            exp_month: 08,
+            exp_year: 2018,
+            cvc: 294
+        };
+        chai.request(server)
+            .post('/parse/functions/createStripeToken')
+            .set('X-Parse-Application-Id', 'emotifAppId',
+                 'X-Parse-REST-API-Key', 'emotifRestKey',
+                 'Content-Type', 'application/json')
+            .send(card)
+            .end((err, res) => {
+                res.should.have.status(200);
+                res.body.should.be.a('object');
+                res.body.should.have.property('result');
+                done();
+            });
+    });
+});
 
+// Error: Trying to create stripe customer for someone who already has one
+describe('/POST /functions/createStripeCustomer', () => {
+    it('it should successfully create a stripe customer', (done) => {
+        let card = {
+            email: 'test5@gmail.com',
+            number: 4242424242424242,
+            exp_month: 08,
+            exp_year: 2018,
+            cvc: 294
+        };
+        chai.request(server)
+            .post('/parse/functions/createStripeToken')
+            .set('X-Parse-Application-Id', 'emotifAppId',
+                 'X-Parse-REST-API-Key', 'emotifRestKey',
+                 'Content-Type', 'application/json')
+            .send(card)
+            .end((err, res) => {
+                res.should.have.status(202);
+                res.body.should.be.a('object');
+                res.body.should.have.property('result');
+                res.body.result.should.have.property('stripeId');
+                done();
+            });
+    });
+});
 
+// Error: Creating stripe transaction for customer without stripe account
+describe('/POST /functions/createTransaction', () => {
+    it('it should give an error that there is no stripe token', (done) => {
+        let transaction = {
+            email: 'josh1@gmail.com',
+            amount: 500
+        }
+        chai.request(server)
+            .post('/parse/functions/createTransaction')
+            .set('X-Parse-Application-Id', 'emotifAppId',
+                 'X-Parse-REST-API-Key', 'emotifRestKey',
+                 'Content-Type', 'application/json')
+            .send(transaction)
+            .end((err, res) => {
+                res.should.have.status(202);
+                res.body.should.be.a('object');
+                res.body.should.have.property('result');
+                done();
+            });
+    });
+});
